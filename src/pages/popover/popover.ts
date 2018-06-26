c34544bb6ea608aca148bd1eb0b954c41b91eb87
fabioepb <djfabiokk@gmail.com>
2018-06-25 22:49:06 -0400

    Fixed PopOvers

--- src/pages/popover/popover.ts
+++ src/pages/popover/popover.ts
@@ -3,7 +3,7 @@ import { NavController } from 'ionic-angular';
 import { LoginPage } from '../login/login';
 
 @Component({
-    template: "popover.html"
+    templateUrl: "popover.html"
 })
 export class PopoverPage{
     constructor(public navCtlr: NavController){}
