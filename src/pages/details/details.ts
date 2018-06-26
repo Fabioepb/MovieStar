185e5bd94f1bb0361c47badb84c421fad19e22e6
fabioepb <djfabiokk@gmail.com>
2018-06-25 22:23:01 -0400

    Reworked UI, Details page Fixed but still in BETA

--- src/pages/details/details.ts
+++ src/pages/details/details.ts
@@ -8,17 +8,30 @@ import { OmdbApi } from '../../api/omdb';
     templateUrl: "details.html"
 })
 export class DetailsPage{
+
     movieId:any;
     movieData:any;
+
     constructor(
         public navParams: NavParams,
         public navCtrl: NavController,
         public api: OmdbApi
     ){
         this.movieId = this.navParams.data;
+
+        this.movieData ={
+            Poster:"",
+            Title: "PLACEHOLDER TITLE",
+            Plot: "N/A",
+            Year: "N/A",
+            Actors: "N/A",
+            Genre: "N/A",
+            imdbRating: "N/A"
+        }
+        
     };
 
-    ionViewCanEnter(){
+    ionViewWillEnter(){
         try{
             this.api.getMovieById(this.movieId)
                 .subscribe(movie=>{
--- src/pages/popover/popover.ts
+++ src/pages/popover/popover.ts
@@ -3,16 +3,7 @@ import { NavController } from 'ionic-angular';
 import { LoginPage } from '../login/login';
 
 @Component({
-    template: `
-        <ion-list>
-            <ion-item>
-                <div (click)="toLogin()">Logout</div>
-            </ion-item>
-            <ion-item>
-                <div>Quit</div>
-            </ion-item>
-        </ion-list>
-        `
+    template: "popover.html"
 })
 export class PopoverPage{
     constructor(public navCtlr: NavController){}
